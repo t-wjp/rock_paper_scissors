2f35833b0b9dafa753dc97175c28b48cb9a2d09e
t-wjp <99971234+t-wjp@users.noreply.github.com>
2024-11-01 20:41:28 +1030

    Added human RPS function

--- rps.js
+++ rps.js
@@ -1,5 +1,3 @@
-console.log("Hello World") 
-
 function getComputerChoice() {
     let max = 4;
     let min = 1
@@ -7,11 +5,29 @@ function getComputerChoice() {
     let choice = "Rock";
     if (result == 2) {
         choice = "Paper";
-        }
+    }
     if (result == 3) {
         choice = "Scissors";
-        }
-    return choice;
     }
+    return choice;
+}
 
+
+function getHumanChoice() {
+    let error_flag = 0;
+    while (error_flag != 1) {  
+        let choice = prompt("What is your selection?")
+        if (choice == "Rock" || choice == "Paper" || choice == "Scissors") {
+            error_flag = 1;
+        }
+        else {
+            choice = prompt("Enter a valid selection?");
+        }
+    }
+    return choice;
+}
+
+
+console.log("Hello World") 
 console.log(getComputerChoice())
+console.log(getHumanChoice())
