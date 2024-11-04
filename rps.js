c6d688c7122c4fb217637f964a45e0dcc001af3c
t-wjp <99971234+t-wjp@users.noreply.github.com>
2024-11-04 21:01:12 +1030

    Added full game function, project complete

--- rps.js
+++ rps.js
@@ -1,16 +1,14 @@
-let humanScore = 0;
-let computerScore = 0;
 
 function getComputerChoice() {
     let max = 4;
     let min = 1
     let result = Math.floor(Math.random() * (max - min)) + min;
-    let choice = "Rock";
+    let choice = "rock";
     if (result == 2) {
-        choice = "Paper";
+        choice = "paper";
     }
     if (result == 3) {
-        choice = "Scissors";
+        choice = "scissors";
     }
     return choice;
 }
@@ -31,30 +29,44 @@ function getHumanChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
-    let humanScore = 0;
-    let computerScore = 0;
-    let gameOver = 0;
-    // let winner = "";
-    while (gameOver != 1) {  
+    while (true) {  
         if ((humanChoice == "rock" && computerChoice == "scissors") ||
             (humanChoice == "paper" && computerChoice == "rock") ||
             (humanChoice == "scissors" && computerChoice == "paper")) {
                 console.log("You win! " + humanChoice + " beats " + computerChoice);
-                humanScore += 1;
-                gameOver = 1;
+                return "Human";
         } else if (humanChoice == computerChoice) {
-                winner = "Tie";
-                gameOver = 1;
+                console.log("Draw!");
+                return "Tie";
         } else {
-                computerScore += 1
-                console.log("You lose! " + computerChoice + " beats " + humanChoice)
-                gameOver = 1;
+                console.log("You lose! " + computerChoice + " beats " + humanChoice);
+                return "Computer";
         }
     }
 }
 
+function playGame() {
+    let round = 5;
+    let humanScore = 0;
+    let computerScore = 0;
+    while (round > 0) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        round -= 1;
+        let result = playRound(humanSelection, computerSelection);
+        if (result === "Human") {
+            humanScore += 1;
+        } else if (result === "Computer") {
+            computerScore += 1;
+        }
+    }
+    if (humanScore > computerScore) {
+        console.log("You win. Your score was " + humanScore + ", while the computer's score was " + computerScore);
+    } else if (humanScore < computerScore) {
+        console.log("You lose. Your score was " + humanScore + ", while the computer's score was " + computerScore);
+    } else {
+        console.log("Draw. You both scored " + humanScore + ".");
+    }
+}
 
-console.log("Hello World");
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-playRound(humanSelection, computerSelection);
+playGame()
